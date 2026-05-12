d8d536dcfa41d033c16d6a8754f1964e4edf8e41
humaira-liza <humairaliza121@gmail.com>
2026-05-12 18:04:22 +0600

    fix order request

--- src/main/java/com/myshop/backend/dto/OrderRequest.java
+++ src/main/java/com/myshop/backend/dto/OrderRequest.java
@@ -3,9 +3,17 @@ package com.myshop.backend.dto;
 import java.util.List;
 
 public class OrderRequest {
+
     public String name;
+
     public String phone;
+
     public String address;
+
     public Double totalAmount;
+
+    // 🔥 ADD THIS
+    public String paymentMethod;
+
     public List<OrderItemRequest> items;
 }
